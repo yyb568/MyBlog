7b8fdf063050b0e922d5fc99210fcb446878d262
yyb5683 <846373066@qq.com>
2017-11-10 23:45:21 +0800

    dd

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,24 +8,8 @@
 if (ENVIRONMENT != 'product'){
 	ini_set('memory_limit', '256M');
 }
-include_once "wxBizMsgCrypt.php";
-define('token','yinyibin');
-define('timeStamp','1510328504');
-define('nonce','sdaf423sain');
-define('appId','wx7a01285bc391466a');
-define('text','<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>');
-define('encodingAesKey','fYPvWtvbggLtKqeMUtDSZnuHYY9IIdA1eFeyvvbGLkg');
 class MY_Controller extends CI_Controller{
-	// protected $APPSECRET = '02211677d75de2aae108644b815d8761';
-	// protected $encodingAesKey = "fYPvWtvbggLtKqeMUtDSZnuHYY9IIdA1eFeyvvbGLkg";
-	// protected $token = "yinyibin";
-	// protected $timeStamp = time();
-	// protected $nonce = "fhe080df9df4df";
-	// protected $appId = "wx7a01285bc391466a";
-	// protected $text = "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>";
-	// 第三方发送消息给公众平台
 	
-
 	/**
 	 * 初始化操作
 	 * add by zhixiao476@gmail.com
@@ -33,36 +17,6 @@ class MY_Controller extends CI_Controller{
 	 */
 	public function __construct(){
 		parent::__construct();
-	}
-
-	public function getWeXinState(){
-		$pc = new WXBizMsgCrypt(token, encodingAesKey, appId);
-		$encryptMsg = '';
-		$errCode = $pc->encryptMsg(text, timeStamp, nonce, encryptMsg);
-		if ($errCode == 0) {
-		print("加密后: " . $encryptMsg . "\n");
-		} else {
-		print($errCode . "\n");
-		}
-
-		$xml_tree = new DOMDocument();
-		$xml_tree->loadXML($this->encryptMsg);
-		$array_e = $xml_tree->getElementsByTagName('Encrypt');
-		$array_s = $xml_tree->getElementsByTagName('MsgSignature');
-		$encrypt = $array_e->item(0)->nodeValue;
-		$msg_sign = $array_s->item(0)->nodeValue;
-
-		$format = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[%s]]></Encrypt></xml>";
-		$from_xml = sprintf($format, $encrypt);
-
-		// 第三方收到公众号平台发送的消息
-		$msg = '';
-		$errCode = $pc->decryptMsg($msg_sign, $timeStamp, nonce, $from_xml, $msg);
-		if ($errCode == 0) {
-			print("解密后: " . $msg . "\n");
-		} else {
-			print($errCode . "\n");
-		}
 	}
 	
 }
