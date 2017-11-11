62615ba9145a2d96598926a7dd5087ec292f1206
yyb <846373066@qq.com>
2017-11-11 12:08:20 +0800

    d

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -55,60 +55,60 @@ class Home extends MY_Controller {
         }
     }
 
-    // public function responseMsg()
-    // {
-    //     $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-    //     if (!empty($postStr)){
-    //         $this->logger("R ".$postStr);
-    //         $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-    //         $RX_TYPE = trim($postObj->MsgType);
-
-    //         switch ($RX_TYPE)
-    //         {
-    //             case "event":
-    //                 $result = $this->receiveEvent($postObj);
-    //                 break;
-    //             case "text":
-    //                 $result = $this->receiveText($postObj);
-    //                 break;
-    //         }
-    //         $this->logger("T ".$result);
-    //         echo $result;
-    //     }else {
-    //         echo "";
-    //         exit;
-    //     }
-    // }
-        public function responseMsg()
+    public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
         if (!empty($postStr)){
+            $this->logger("R ".$postStr);
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "？")
+            $RX_TYPE = trim($postObj->MsgType);
+
+            switch ($RX_TYPE)
             {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
+                case "event":
+                    $result = $this->receiveEvent($postObj);
+                    break;
+                case "text":
+                    $result = $this->receiveText($postObj);
+                    break;
             }
-        }else{
+            $this->logger("T ".$result);
+            echo $result;
+        }else {
             echo "";
             exit;
         }
     }
+    //     public function responseMsg()
+    // {
+    //     $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+    //     if (!empty($postStr)){
+    //         $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+    //         $fromUsername = $postObj->FromUserName;
+    //         $toUsername = $postObj->ToUserName;
+    //         $keyword = trim($postObj->Content);
+    //         $time = time();
+    //         $textTpl = "<xml>
+    //                     <ToUserName><![CDATA[%s]]></ToUserName>
+    //                     <FromUserName><![CDATA[%s]]></FromUserName>
+    //                     <CreateTime>%s</CreateTime>
+    //                     <MsgType><![CDATA[%s]]></MsgType>
+    //                     <Content><![CDATA[%s]]></Content>
+    //                     <FuncFlag>0</FuncFlag>
+    //                     </xml>";
+    //         if($keyword == "?" || $keyword == "？")
+    //         {
+    //             $msgType = "text";
+    //             $contentStr = date("Y-m-d H:i:s",time());
+    //             $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+    //             echo $resultStr;
+    //         }
+    //     }else{
+    //         echo "";
+    //         exit;
+    //     }
+    // }
     
     private function receiveEvent($object)
     {
