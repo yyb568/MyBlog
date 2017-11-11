99b693e2b99525c5ddf497334671dd44b07386f2
yyb <846373066@qq.com>
2017-11-11 09:06:31 +0800

    sad

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,7 +5,6 @@
  * add by yyb5683@gmail.com
  * 2017年6月12日16:31:04
  */
-define('TOKEN','yinyibin');
 class Home extends MY_Controller {
 
 	/**
@@ -20,25 +19,32 @@ class Home extends MY_Controller {
 	
 
 	public function index(){
-		//初次接入验证
-		$this->valid();
+		
+		// $this->valid();
 		// $this->load->view('home/index');
+
+		if (!isset($_GET['echostr'])) {
+			$this->responseMsg();
+		}else{
+			//初次接入验证
+    		$this->valid();
+		}
 	}
 
-		/**
+	/**
 	 * 首次接入验证
 	 * add by  yyb5683@gmail.com
 	 * 2017年6月14日09:13:51
 	 */
-	public function valid(){
-		$echoStr = $_GET["echostr"];
+	// public function valid(){
+	// 	$echoStr = $_GET["echostr"];
 	
-		//valid signature , option
-		if($this->checkSignature()){
-			echo $echoStr;
-			exit;
-		}
-	}
+	// 	//valid signature , option
+	// 	if($this->checkSignature()){
+	// 		echo $echoStr;
+	// 		exit;
+	// 	}
+	// }
 
 
 	/**
@@ -46,22 +52,22 @@ class Home extends MY_Controller {
 	 * add by  yyb5683@gmail.com
 	 * 2017年6月14日09:13:51
 	 */
-	private function checkSignature(){
-		$signature = $_GET["signature"];
-		$timestamp = $_GET["timestamp"];
-		$nonce = $_GET["nonce"];
+	// private function checkSignature(){
+	// 	$signature = $_GET["signature"];
+	// 	$timestamp = $_GET["timestamp"];
+	// 	$nonce = $_GET["nonce"];
 	
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-		sort($tmpArr);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
+	// 	$token = TOKEN;
+	// 	$tmpArr = array($token, $timestamp, $nonce);
+	// 	sort($tmpArr);
+	// 	$tmpStr = implode( $tmpArr );
+	// 	$tmpStr = sha1( $tmpStr );
 	
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	// 	if( $tmpStr == $signature ){
+	// 		return true;
+	// 	}else{
+	// 		return false;
+	// 	}
+	// }
 }
 
